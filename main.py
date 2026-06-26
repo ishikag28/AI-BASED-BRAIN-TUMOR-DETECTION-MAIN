485180b5d56ea91269ad6a132ed5efa981fc8351
Ishika Gupta <164764440+ishikag28@users.noreply.github.com>
2026-06-27 03:14:39 +0530

    Update app.run to use environment port

--- main.py
+++ main.py
@@ -92,4 +92,5 @@ def uploaded_file(filename):
 # RUN
 # -------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
