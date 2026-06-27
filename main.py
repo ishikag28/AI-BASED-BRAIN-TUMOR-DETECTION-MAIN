1222c9d4754b464b3d3e8a332214fe425cbc1ab1
Ishika Gupta <164764440+ishikag28@users.noreply.github.com>
2026-06-27 13:28:12 +0530

    Change default port from 7860 to 10000

--- main.py
+++ main.py
@@ -92,5 +92,5 @@ def uploaded_file(filename):
 # RUN
 # -------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
