5155bc8eed182eedad6fc9d19281a32476afb45a
Ishika Gupta <164764440+ishikag28@users.noreply.github.com>
2026-06-27 13:27:13 +0530

    Add fuzzy option to gdown download

--- download_model.py
+++ download_model.py
@@ -8,6 +8,6 @@ MODEL_PATH = "models/model.h5"
 if not os.path.exists(MODEL_PATH):
     print("Downloading model...")
     url = "https://drive.google.com/uc?id=10nGYGyXCUJI8Nbz3hPpRDb9H26lQeG8A"
-    gdown.download(url, MODEL_PATH, quiet=False)
+    gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
 else:
     print("Model already exists.")
